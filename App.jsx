5dbf3f47277b0361979e60c646fb90354913a873
udkumar <udkumar@hotmail.com>
2021-11-15 20:44:22 +0530

    text updated in index

--- App.jsx
+++ App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class App extends React.Component {
   render() {
     return (
-      <div>simon, helloworld!!!</div>
+      <div>uday, helloworld!!!</div>
     );
   }
 }
